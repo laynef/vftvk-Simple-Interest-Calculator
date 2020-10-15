86cc07523b78b9e2320f26ae138837c18832e60e
laynef <laynefaler@gmail.com>
2020-10-15 14:45:00 -0700

    Setup styling for project

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var p = document.getElementById("principal").value;
+    var r = document.getElementById("rate").value;
+    var n = document.getElementById("years").value;
+    var total = 
 }
-        
+
+var output = document.getElementById("rate-output");
+var slider = document.getElementById("rate");
+
+output.innerHTML = slider.value;
+slider.oninput = function() {
+    output.innerHTML = this.value;
+}
