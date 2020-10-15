799b701ce108733be31ee171d69aeede0573fec3
laynef <laynefaler@gmail.com>
2020-10-15 15:17:12 -0700

    Added javascript for compution

--- script.js
+++ script.js
@@ -1,9 +1,31 @@
+function createElementFromHTML(htmlString) {
+    var div = document.createElement('div');
+    div.innerHTML = htmlString.trim();
+    return div.firstChild; 
+}
+
+
 function compute()
 {
-    var p = document.getElementById("principal").value;
-    var r = document.getElementById("rate").value;
-    var n = document.getElementById("years").value;
-    var total = 
+    var p = Number(document.getElementById("principal").value);
+    var r = Number(document.getElementById("rate").value) / 100;
+    var t = Number(document.getElementById("years").value);
+    var total = p * r * t;
+
+    var date = new Date();
+    var y = date.getFullYear() + t;
+    
+    var results = document.getElementById('result');
+    results.appendChild(
+        createElementFromHTML(`
+            <p>
+                If you deposit ${p}<br>
+                at an interest rate of ${r}%.<br>
+                You will receive an amount of ${total}<br>
+                in year ${y}
+            </p>
+        `)
+    )
 }
 
 var output = document.getElementById("rate-output");
