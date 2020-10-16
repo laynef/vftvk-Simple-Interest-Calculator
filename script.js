5f06c7f41bb0e5d4c2166c20bdefd2d05ccc8760
laynef <laynefaler@gmail.com>
2020-10-15 18:04:59 -0700

    Added changes for checklist

--- script.js
+++ script.js
@@ -10,8 +10,11 @@ function createElementFromHTML(htmlString)
 // On click handler
 function compute()
 {
+    var results = document.getElementById("result");
+
     var p = Number(document.getElementById("principal").value);
-    var r = Number(document.getElementById("rate").value) / 100;
+    var i = Number(document.getElementById("rate").value);
+    var r = i / 100;
     var t = Number(document.getElementById("years").value);
     var total = p * r * t;
 
@@ -20,16 +23,17 @@ function compute()
         return;
     }
 
+    results.innerHTML = '';
+
     var date = new Date();
     var y = date.getFullYear() + t;
     
-    var results = document.getElementById("result");
     results.appendChild(
         createElementFromHTML(
-            "<p>If you deposit " + 
-            p + " <br> at an interest rate of " + 
-            r + " %.<br> You will receive an amount of " + 
-            total + " <br> in year " + y + " </p>"
+            "<p>If you deposit <span class=\"text-yellow\">" + 
+            p + "</span>. <br> at an interest rate of <span class=\"text-yellow\">" + 
+            i + " %</span>.<br> You will receive an amount of <span class=\"text-yellow\">" + 
+            total + "</span> <br> in year <span class=\"text-yellow\">" + y + "</span> </p>"
         )
     )
 }
